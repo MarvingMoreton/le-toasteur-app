71f23d503f02f654bc1281244990de6d644d5015
Marving Moreton <moreton.marving@gmail.com>
2022-10-13 12:37:52 -0400

    navbar v0

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -2,8 +2,11 @@ import classes from "./MainNavigation.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { useRouter } from "next/router";
 
 function MainNavigation() {
+  const router = useRouter();
+  const currentRoute = router.pathname;
   return (
     <React.Fragment>
       <header>
@@ -18,43 +21,69 @@ function MainNavigation() {
             />
           </a>
         </Link>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">
-                <a className={classes["x"]}>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a className={classes["x"]}>Menu</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a className={classes["x"]}>Photos</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>
-                  <Image
-                    src="/images/toast-blue-navy.png"
-                    alt="test"
-                    // layout="fill"
-                    width={124}
-                    height={124}
-                  />
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a className={classes["x"]}>Notre Histoire</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <div className={classes["nav-container"]}>
+          <nav className={classes["main-nav"]}>
+            <ul className={classes["main-nav-list"]}>
+              <li>
+                <Link href="/">
+                  <a
+                    className={`${classes["main-nav-link"]} ${
+                      currentRoute === "/" ? "active" : ""
+                    }`}
+                  >
+                    Home
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a
+                    className={`${classes["main-nav-link"]} ${
+                      currentRoute === "/" ? "active" : ""
+                    }`}
+                  >
+                    Menu
+                  </a>
+                </Link>
+              </li>
+              <li className={classes["image-toast"]}>
+                <Link href="/">
+                  <a>
+                    <Image
+                      src="/images/toast-blue-navy.png"
+                      alt="test"
+                      // layout="fill"
+                      width={124}
+                      height={124}
+                    />
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a
+                    className={`${classes["main-nav-link"]} ${
+                      currentRoute === "/" ? "active" : ""
+                    }`}
+                  >
+                    Photos
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a
+                    className={`${classes["main-nav-link"]} ${
+                      currentRoute === "/" ? "active" : ""
+                    }`}
+                  >
+                    Notre Histoire
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
       <hr className={classes.line}></hr>
     </React.Fragment>
