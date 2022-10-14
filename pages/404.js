5ab9b4fbc8ff4454eea256d1fe734cfdca07a9ca
Marving Moreton <moreton.marving@gmail.com>
2022-10-14 17:01:41 -0400

    faq page - V0

--- pages/404.js
+++ pages/404.js
@@ -3,9 +3,12 @@ import Head from "next/head";
 
 import Link from "next/link";
 import Image from "next/image";
+import classes from "../components/ui/error.module.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function NotFoundPage() {
   return (
@@ -15,7 +18,23 @@ function NotFoundPage() {
         <meta name="description" content="Meta description to be filled!" />
       </Head>
 
-      <h1>TBD</h1>
+      <div className={classes["background"]}>
+        <div className={classes["box-text"]}>
+          <span>erreur</span>
+          <h1>404</h1>
+          <h2>Quelque chose a mal tourné </h2>
+          <div className={classes["box-link"]}>
+            <Link href="/">
+              <a className={classes["arrow-icon"]}>
+                <FontAwesomeIcon icon={faArrowLeft} />
+              </a>
+            </Link>
+            <Link href="/">
+              <a>Back</a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
