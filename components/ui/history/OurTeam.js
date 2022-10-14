b6b8348df6427b1d18d093e92db9f693da06b43d
Marving Moreton <moreton.marving@gmail.com>
2022-10-14 15:39:15 -0400

    Our Team component - V0

---
+++ components/ui/history/OurTeam.js
@@ -0,0 +1,280 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import classes from "./OurTeam.module.css";
+
+function OurTeam(props) {
+  return (
+    <section className={`${classes["section-team"]} ${"container"}`}>
+      <div className={classes["grid-team"]}>
+        <div className={classes["box-text"]}>
+          <span className={classes.subtitle}>La Dream Team</span>
+          <h2>Notre équipe</h2>
+        </div>
+        <div className={classes["team-item-list"]}>
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 2 */}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 3 */}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 4 */}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 5 */}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 6*/}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 7*/}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 8*/}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 9*/}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+          {/* EMPLOYEE CARD 10*/}
+          <div className={classes["employee-card"]}>
+            <div className={classes["image-box"]}>
+              <Image
+                src="/images/employee-dummy.png"
+                alt="test"
+                // layout="fill"
+                width={399}
+                height={470}
+              />
+            </div>
+            <div className={classes["employee-text-box"]}>
+              <h4>John Doe</h4>
+              <span className={classes.position}>Serveuse</span>
+              <p>
+                Après avoir travaillé plusieurs années dans différentes
+                boucheries, Félix est maintenant bien en selle chez PLB depuis
+                plusieurs années.
+              </p>
+              <p>
+                Étant un artiste dans l’âme et soucieux du détail, il est
+                souvent derrière plusieurs confections raffinées que vous
+                trouverez dans notre vitrine au fil des saisons.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default OurTeam;
--- pages/notre-histoire.js
+++ pages/notre-histoire.js
@@ -5,13 +5,14 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/ui/shared/Banner";
 import ImageFeatured from "../components/ui/history/ImageFeatured";
 import Values from "../components/ui/history/Values";
+import OurTeam from "../components/ui/history/OurTeam";
 
 export default function OurStory(props) {
   return (
     <div>
       <Banner />
       <ImageFeatured />
-      <Values />
+      <OurTeam />
     </div>
   );
 }
