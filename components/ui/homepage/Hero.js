cf7843b9d4155645fe496df8fae4fc229e73890a
Marving Moreton <moreton.marving@gmail.com>
2022-10-13 21:28:42 -0400

    component featured image done

--- components/ui/homepage/Hero.js
+++ components/ui/homepage/Hero.js
@@ -6,28 +6,22 @@ import React from "react";
 function Hero() {
   return (
     <section className={classes["hero-section"]}>
-      <div>
+      <div className={classes["box-text"]}>
         <h1>Le meilleur Brunch de l’île</h1>
         <span>Profiter de notre délicieux brunch</span>
         <a>Notre menu</a>
-        <div></div>
+        <a>Notre menu</a>
       </div>
-      <Image
-        className={classes["hero-overlay"]}
-        src="/images/white-overlay.png"
-        alt="test"
-        // layout="fill"
-        width={638}
-        height={549}
-      />
       <div className={classes["image-box"]}>
         <Image
           className={classes["hero-illustration"]}
-          src="/images/breakfast.png"
+          src="/images/illustration-overlayed.png"
           alt="test"
           // layout="responsive"
-          layout="fill"
+          // layout="fill"
           objectFit="contain"
+          width={638}
+          height={549}
         />
       </div>
     </section>
--- components/ui/homepage/ImageFeatured.js
+++ components/ui/homepage/ImageFeatured.js
@@ -5,8 +5,8 @@ import React from "react";
 
 function ImageFeatured() {
   return (
-    <section className={classes["section-featured"]}>
-      <div>
+    <section className={`${classes["section-featured"]} ${"container"}`}>
+      <div className={classes["box-featured-image"]}>
         <Image
           src="/images/expert-brunch.png"
           alt="test"
@@ -18,23 +18,30 @@ function ImageFeatured() {
       <div className={classes["box-text"]}>
         <span className={classes.subtitle}>Expert en Brunch</span>
         <h2 className={classes.title}>Le spécialiste des Brunchs</h2>
-        <p>
-          The point of using Lor It is a long established fact that a reader
-          will be distracted by the readable content of a page when looking at
-          its layout.
-        </p>
-        <br></br>
-        <p>
-          The point of using Lor It is a long established fact that a reader
-          will be distracted by the readable content of a page when looking at
-          its layout.
-        </p>
-        <div>
-          <Link href="">
-            <a>Notre menu</a>
+        <div className={classes["box-paragraph"]}>
+          <p>
+            The point of using Lor It is a long established fact that a reader
+            will be distracted by the readable content of a page when looking at
+            its layout.
+          </p>
+          <p>
+            The point of using Lor It is a long established fact that a reader
+            will be distracted by the readable content of a page when looking at
+            its layout.
+          </p>
+
+          <p>
+            The point of using Lor It is a long established fact that a reader
+            will be distracted by the readable content of a page when looking at
+            its layout.
+          </p>
+        </div>
+        <div className={classes["box-buttons"]}>
+          <Link href="/menu">
+            <a className="btn-primary">Notre menu</a>
           </Link>
-          <Link href="">
-            <a>Notre Histoire</a>
+          <Link href="/notre-histoire">
+            <a className="btn-secondary">Notre Histoire</a>
           </Link>
         </div>
       </div>
