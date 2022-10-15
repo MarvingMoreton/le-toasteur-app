ebc67651f724c0aeaf0caf3d6e76f53ff2b0fc0b
Marving Moreton <moreton.marving@gmail.com>
2022-10-15 16:27:34 -0400

    gallery list - basic zoom effect

--- components/ui/Gallery/GalleryList.js
+++ components/ui/Gallery/GalleryList.js
@@ -1,82 +1,124 @@
 import classes from "./GalleryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import galleryOne from "../../../public/images/illustrations/gallery--1.jpg";
+import galleryTwo from "../../../public/images/illustrations/gallery--2.jpg";
+import galleryThree from "../../../public/images/illustrations/gallery--3.jpg";
+import galleryFour from "../../../public/images/illustrations/gallery--4.jpg";
+import galleryFive from "../../../public/images/illustrations/gallery--5.jpg";
+import gallerySix from "../../../public/images/illustrations/gallery--6.jpg";
+import gallerySeven from "../../../public/images/illustrations/gallery--7.jpg";
+
 import React from "react";
+
 function GalleryList(props) {
   return (
     <div className={`${classes["section-gallery"]} ${"container"}`}>
       <div className={classes["gallery"]}>
+        {/* FIGURE */}
         <figure className={classes["gallery-item"]}>
-          <Image
-            src="/images/office-image.png"
-            alt="test"
-            // layout="fill"
-            width={450}
-            height={450}
-          />
+          <div className={`${classes["image-box"]} ${classes["zoom"]}`}>
+            <Image
+              className="image"
+              src={galleryOne}
+              alt="test"
+              layout="responsive"
+              // width={500}
+              // height={500}
+            />
+          </div>
         </figure>
+
+        {/* FIGURE */}
         <figure className={classes["gallery-item"]}>
-          <Image
-            src="/images/office-image.png"
-            alt="test"
-            // layout="fill"
-            width={450}
-            height={450}
-          />
+          <div className={`${classes["image-box"]} ${classes["zoom"]}`}>
+            <Image
+              className="image"
+              src={galleryTwo}
+              alt="test"
+              // layout="fill"
+              // width={450}
+              // height={450}
+            />
+          </div>
         </figure>
+        {/* FIGURE */}
         <figure className={classes["gallery-item"]}>
-          <Image
-            src="/images/office-image.png"
-            alt="test"
-            // layout="fill"
-            width={450}
-            height={450}
-          />
+          <div className={`${classes["image-box"]} ${classes["zoom"]}`}>
+            <Image
+              className="image"
+              src={galleryThree}
+              alt="test"
+              // layout="fill"
+              // width={450}
+              // height={450}
+            />
+          </div>
         </figure>
+        {/* FIGURE */}
         <figure className={classes["gallery-item"]}>
-          <Image
-            src="/images/office-image.png"
-            alt="test"
-            // layout="fill"
-            width={450}
-            height={450}
-          />
+          <div className={`${classes["image-box"]} ${classes["zoom"]}`}>
+            <Image
+              className="image"
+              src={galleryFour}
+              alt="test"
+              // layout="fill"
+              // width={450}
+              // height={450}
+            />
+          </div>
         </figure>
+        {/* FIGURE */}
         <figure className={classes["gallery-item"]}>
-          <Image
-            src="/images/office-image.png"
-            alt="test"
-            // layout="fill"
-            width={450}
-            height={450}
-          />
+          <div className={`${classes["image-box"]} ${classes["zoom"]}`}>
+            <Image
+              className="image"
+              src={galleryFive}
+              alt="test"
+              // layout="fill"
+              // width={450}
+              // height={450}
+            />
+          </div>
         </figure>
+        {/* FIGURE */}
         <figure className={classes["gallery-item"]}>
-          <Image
-            src="/images/office-image.png"
-            alt="test"
-            // layout="fill"
-            width={450}
-            height={450}
-          />
+          <div className={`${classes["image-box"]} ${classes["zoom"]}`}>
+            <Image
+              className="image"
+              src={gallerySix}
+              alt="test"
+              // layout="fill"
+              // width={450}
+              // height={450}
+            />
+          </div>
         </figure>
+        {/* FIGURE */}
         <figure className={classes["gallery-item"]}>
-          <Image
-            src="/images/office-image.png"
-            alt="test"
-            // layout="fill"
-            width={450}
-            height={450}
-          />
+          <div className={`${classes["image-box"]} ${classes["zoom"]}`}>
+            <Image
+              className="image"
+              src={gallerySeven}
+              alt="test"
+              // layout="fill"
+              // width={450}
+              // height={450}
+            />
+          </div>
         </figure>
+        {/* FIGURE */}
         <figure className={classes["gallery-item"]}>
-          <Image
-            src="/images/office-image.png"
-            alt="test"
-            // layout="fill"
-            width={450}
-            height={450}
-          />
+          <div className={`${classes["image-box"]} ${classes["zoom"]}`}>
+            <Image
+              className="image"
+              src={galleryTwo}
+              alt="test"
+              // layout="fill"
+              // width={450}
+              // height={450}
+            />
+          </div>
         </figure>
       </div>
       <div className={classes["btn-box"]}>
