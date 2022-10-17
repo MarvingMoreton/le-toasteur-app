ec6735fccf465097f1abf60e4ffac352824f37fa
Marving Moreton <moreton.marving@gmail.com>
2022-10-17 10:36:56 -0400

    embedded map

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -45,13 +45,23 @@ function Footer() {
             </div>
           </div>
           <div className={classes["footer-col--3"]}>
+            <iframe
+              className={classes["map"]}
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.3112351467425!2d-73.62349344916144!3d45.54406423602973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9193d3612eac7%3A0x574f4f87d020617!2sLe%20Toasteur%20Villeray!5e0!3m2!1sfr!2sca!4v1666017198638!5m2!1sfr!2sca"
+              width="500"
+              height="350"
+              // style="border:0;"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
             {/* Map - placeholder */}
-            <Image
+            {/* <Image
               src="/images/map-placeholder.png"
               alt="test"
               width={436}
               height={311}
-            />
+            /> */}
           </div>
           <div className={classes["footer-col--4"]}>
             <span className={classes.subtitle}>Services</span>
