96f119afa548f898100f0e3f25d762b2990991e9
Marving Moreton <moreton.marving@gmail.com>
2022-10-19 19:56:08 -0400

    responsiveness impact content component

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
     <div>
       <Hero />
       <ImageFeatured />
-      <ImpactContent />
       <BookTable />
+      <ImpactContent />
       <Testimonials />
       <br></br>
       <TestimonialsGallery />
