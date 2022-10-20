b023cf603e26d1327f95ad6e69889b5f41d51067
Marving Moreton <moreton.marving@gmail.com>
2022-10-20 00:33:42 -0400

    impactcontent component responsiveness v2

--- components/ui/homepage/ImpactContent.js
+++ components/ui/homepage/ImpactContent.js
@@ -6,17 +6,19 @@ import React from "react";
 function ImpactContent() {
   return (
     <section className={classes["section-impact-content"]}>
-      <div className={`${classes["box-content"]} ${"container"}`}>
-        <h2 className={classes["title-impact"]}>
-          Notre nourriture est faite à{" "}
-          <span className={classes["title-impact-highlighted"]}>
-            100% avec amour!
+      <div className="container">
+        <div className={classes["box-content"]}>
+          <h2 className={classes["title-impact"]}>
+            Notre nourriture est faite à{" "}
+            <span className={classes["title-impact-highlighted"]}>
+              100% avec amour!
+            </span>
+          </h2>
+          <span className={classes["small-text"]}>
+            Toutes nos préparations sont faite à partir de produits frais en
+            travaillant avec des producteurs locaux au maximum.
           </span>
-        </h2>
-        <span className={classes["small-text"]}>
-          Toutes nos préparations sont faite à partir de produits frais en
-          travaillant avec des producteurs locaux au maximum.
-        </span>
+        </div>
       </div>
     </section>
   );
