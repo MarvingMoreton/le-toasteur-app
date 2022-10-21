83450fd0c8819e5401b790ba9652e77936f8eb89
Marving Moreton <moreton.marving@gmail.com>
2022-10-20 21:45:34 -0400

    footer responsiveness v1

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
   return (
     <React.Fragment>
       <footer className={classes["footer"]}>
-        <div className={` ${classes.container} ${classes.grid} `}>
+        <div className={`${classes["footer-grid"]} ${"container"}`}>
           <div className={classes["footer-col--1"]}>
             <div className={classes["image-logo-navy"]}>
               <Image
@@ -47,8 +47,8 @@ function Footer() {
             <iframe
               className={classes["map"]}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.3112351467425!2d-73.62349344916144!3d45.54406423602973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9193d3612eac7%3A0x574f4f87d020617!2sLe%20Toasteur%20Villeray!5e0!3m2!1sfr!2sca!4v1666017198638!5m2!1sfr!2sca"
-              width="480"
-              height="320"
+              // width="480"
+              // height="320"
               // style="border:0;"
               allowfullscreen=""
               loading="lazy"
@@ -56,31 +56,65 @@ function Footer() {
             ></iframe>
           </div>
           <div className={classes["footer-col--4"]}>
-            <span className={classes.subtitle}>Services</span>
-            <ul className={classes["col-4-nav-list"]}>
-              <li>
-                <Link href="/gallerie">
-                  <a>Photos</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/notre-histoire">
-                  <a>Notre Histoire</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contact">
-                  <a>Contact</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="">
-                  <a>English</a>
-                </Link>
-              </li>
-            </ul>
+            <div>
+              <span className={classes.subtitle}>Services</span>
+              <ul className={classes["col-4-nav-list"]}>
+                <li>
+                  <Link href="/gallerie">
+                    <a>Photos</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/notre-histoire">
+                    <a>Notre Histoire</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/contact">
+                    <a>Contact</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="">
+                    <a>English</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className={classes["footer-col--5"]}>
+              <span className={classes.subtitle}>Notre communauté</span>
+              <ul className={classes["col-5-nav-list"]}>
+                <li>
+                  <Link href="https://fr-ca.facebook.com/LeToasteurVilleray/">
+                    <a target="_blank" rel="noopener noreferrer">
+                      <div className={classes["icon-svg"]}>
+                        <Facebook sx={{ fontSize: 30 }} />
+                      </div>
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://www.instagram.com/toasteurvilleray/?hl=fr">
+                    <a target="_blank" rel="noopener noreferrer">
+                      <div className={classes["icon-svg"]}>
+                        <Instagram sx={{ fontSize: 30 }} />
+                      </div>{" "}
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a>
+                      <div className={classes["icon-svg"]}>
+                        <Google sx={{ fontSize: 30 }} />
+                      </div>{" "}
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className={classes["footer-col--5"]}>
+          {/* <div className={classes["footer-col--5"]}>
             <span className={classes.subtitle}>Notre communauté</span>
             <ul className={classes["col-5-nav-list"]}>
               <li>
@@ -111,7 +145,7 @@ function Footer() {
                 </Link>
               </li>
             </ul>
-          </div>
+          </div> */}
         </div>
         <hr className={classes.line}></hr>
       </footer>
