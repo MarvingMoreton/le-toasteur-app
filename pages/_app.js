35a195f8f955d4be5b220f99c6d3a81ba012240e
Marving Moreton <moreton.marving@gmail.com>
2022-10-22 17:34:54 -0400

    Prismic set up - ongoing

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,34 @@ import "../styles/globals.css";
 
 import Layout from "../components/layout/Layout";
 import Head from "next/head";
+import Link from "next/link";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { letoasteur } from "../prismicio";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="/images/favicon-daj.png" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <PrismicProvider
+      internalLinkComponent={({ href, ...props }) => (
+        <Link href={href}>
+          <a {...props} />
+        </Link>
+      )}
+    >
+      <Layout>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" type="image/png" href="/images/favicon-daj.png" />
+        </Head>
+
+        <PrismicPreview repositoryName={letoasteur}>
+          <Component {...pageProps} />
+        </PrismicPreview>
+      </Layout>
+    </PrismicProvider>
   );
 }
 
