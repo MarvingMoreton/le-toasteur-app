c254d6b1fced96709051e129f9cc09ee5dd20add
Marving Moreton <moreton.marving@gmail.com>
2022-10-23 00:06:04 -0400

    prismic set up - V2

---
+++ .slicemachine/assets/slices/HeroSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/HeroSlice';
+
+export default {
+  title: 'slices/HeroSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"cta_link":{"link_type":"Web","url":"https://slicemachine.dev"},"cta_text":"rise"}],"primary":{"title":[{"type":"heading1","text":"Public","spans":[]}],"description":[{"type":"paragraph","text":"Irure ad ad dolor quis aliqua. Occaecat Lorem cupidatat tempor mollit ipsum dolor non in id. Eiusmod quis Lorem excepteur esse dolore dolore incididunt ea sunt do exercitation enim qui.","spans":[]}],"image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1515378791036-0648a3ef77b2"}},"slice_type":"hero_slice","id":"_Default"}} />
+_Default.storyName = ''
---
+++ .slicemachine/assets/slices/ImageGallery/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/ImageGallery';
+
+export default {
+  title: 'slices/ImageGallery'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1600804931749-2da4ce26c869"}}],"primary":{"title":[{"type":"heading1","text":"Reason","spans":[]}],"description":[{"type":"paragraph","text":"Eu nulla Lorem esse eu do laborum non consequat.","spans":[]}]},"slice_type":"image_gallery","id":"_Default"}} />
+_Default.storyName = ''
