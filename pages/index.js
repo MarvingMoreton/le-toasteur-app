2374bfdd478089451cec6717f9979ba33bca9113
Marving Moreton <moreton.marving@gmail.com>
2023-04-20 20:12:00 -0400

    V0 EN version

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,47 @@ import { ReactFragment } from 'react';
 import { createClient } from '../prismicio';
 import { components } from '../slices';
 import TestimonialsBis from '../components/ui/shared/TestimonialsBis';
+import { resolveLocaleFromNext } from '../lib/resolveLocaleFromNext';
+import { withAlternateLanguageURLs } from '../lib/withAlternateLanguageURLs';
+import { useRouter } from 'next/router';
+
 const Page = ({ page, navigation, settings }) => {
   return <SliceZone slices={page.data.slices} components={components} />;
 };
 
 export default Page;
 
-export async function getStaticProps({ previewData }) {
-  const client = createClient({ previewData });
+// export async function getStaticProps({ previewData }) {
+//   const client = createClient({ previewData });
 
-  const page = await client.getSingle('homepage');
+//   const page = await client.getSingle('homepage');
+
+//   return {
+//     props: {
+//       page
+//     }
+//   };
+// }
+
+export async function getStaticProps({ previewData, locale }) {
+  const client = createClient({ previewData });
+  const resolvedLocale = resolveLocaleFromNext(locale);
+
+  const page = await client.getSingle('homepage', {
+    lang: resolvedLocale
+  });
+  const pageWithAlternateLanguageURLs = await withAlternateLanguageURLs(
+    page,
+    client
+  );
 
   return {
     props: {
-      page
+      // metaTitle: page.data.meta_title,
+      // metaDescription: page.data.meta_description,
+      // ogImage: page.data.og_image.url,
+      // ogImageAlt: page.data.og_image.alt,
+      page: pageWithAlternateLanguageURLs
     }
   };
 }
--- prismicio.js
+++ prismicio.js
@@ -1,24 +1,31 @@
-import * as prismic from "@prismicio/client";
-import * as prismicH from "@prismicio/helpers";
-import * as prismicNext from "@prismicio/next";
-import sm from "./sm.json";
+import * as prismic from '@prismicio/client';
+import * as prismicH from '@prismicio/helpers';
+import * as prismicNext from '@prismicio/next';
+import sm from './sm.json';
 
 /**
  * The project's Prismic repository name.
  */
 export const letoasteur = prismic.getRepositoryName(sm.apiEndpoint);
 
+/**
+ * Locale overrides for nicer URLs.
+ */
+export const localeOverrides = {
+  'fr-fr': 'fr'
+};
+
 // Update the routes array to match your project's route structure
 /** @type {prismic.ClientConfig['routes']} **/
 const routes = [
   {
-    type: "homepage",
-    path: "/",
+    type: 'homepage',
+    path: '/'
   },
   {
-    type: "page",
-    path: "/:uid",
-  },
+    type: 'page',
+    path: '/:uid'
+  }
 ];
 
 /**
@@ -27,17 +34,35 @@ const routes = [
  *
  * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
  */
-export const createClient = (config = {}) => {
+export const createClient = ({ previewData, req, ...config } = {}) => {
   const client = prismic.createClient(sm.apiEndpoint, {
     routes,
     ...config,
+    defaultParams: {
+      ...config.defaultParams,
+      lang: '*'
+    }
   });
 
   prismicNext.enableAutoPreviews({
     client,
     previewData: config.previewData,
-    req: config.req,
+    req: config.req
   });
 
   return client;
 };
+// export const createClient = (config = {}) => {
+//   const client = prismic.createClient(sm.apiEndpoint, {
+//     routes,
+//     ...config
+//   });
+
+//   prismicNext.enableAutoPreviews({
+//     client,
+//     previewData: config.previewData,
+//     req: config.req
+//   });
+
+//   return client;
+// };
