e12f086f6de115ffbcdc08b2091607123d7b8d53
Marving Moreton <marving.moreton@gmail.com>
2024-10-14 19:41:56 -0400

    attempt in progress

--- slices/BlogPostList/index.js
+++ slices/BlogPostList/index.js
@@ -102,13 +102,10 @@ const BlogPostList = ({ slice }) => (
               </div>
               <div className="group relative">
                 <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <PrismicLink
-                    href={post.href}
-                    className="hover:text-[#ff5c39]"
-                  >
+                  <a href={post.href} className="hover:text-[#ff5c39]">
                     <span className="absolute inset-0" />
                     {post.title}
-                  </PrismicLink>
+                  </a>
                 </h3>
                 <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
                   {post.description}
